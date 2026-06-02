00546b6ddf95fb0058e460daa56a9d82b1c14992
firmwarecostum <145313986+firmwarecostum@users.noreply.github.com>
2026-06-02 11:33:55 +0800

    Add blocklist for 'niceaddress_union' type

--- build.rs
+++ build.rs
@@ -49,6 +49,7 @@ fn main() {
                 .iter()
                 .map(|path| format!("-I{}", path.to_string_lossy())),
         )
+        .blocklist_type("niceaddress_union")
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
